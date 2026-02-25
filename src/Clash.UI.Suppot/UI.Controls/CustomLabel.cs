6d3e2451912f1742797fef4679ca52f641afab39
晨辰 聂 <2457214063@qq.com>
2026-02-25 17:30:47 +0800

    添加流量统计板

---
+++ src/Clash.UI.Suppot/UI.Controls/CustomLabel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using Label = System.Windows.Controls.Label;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Clash.UI.Suppot.UI.Controls
+{
+    public class CustomLabel:Label
+    {
+
+
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(Geometry), typeof(CustomLabel), new PropertyMetadata(null));
+
+
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(nameof(Header), typeof(string), typeof(CustomLabel), new PropertyMetadata(""));
+
+
+
+
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(nameof(Value), typeof(string), typeof(CustomLabel), new PropertyMetadata("0"));
+
+
+
+
+        public string Unit
+        {
+            get { return (string)GetValue(UnitProperty); }
+            set { SetValue(UnitProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Unit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UnitProperty =
+            DependencyProperty.Register(nameof(Unit), typeof(string), typeof(CustomLabel), new PropertyMetadata(""));
+
+
+
+
+
+    }
+}
