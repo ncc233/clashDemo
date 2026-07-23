a5c05a9c8fa6e0f1fe2c743c048048bbbffc2e1b
晨辰 聂 <2457214063@qq.com>
2026-07-23 17:03:42 +0800

    bogus测试

---
+++ src/ClashDemo/Models/ConnectionPageTestModel.cs
@@ -0,0 +1,27 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashDemo.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ConnectionPageTestModel
+    {
+        [Column("姓名")]
+        public string Name { get; set; }
+        [Column("年龄")]
+        public int Age { get; set; }
+
+        [Column("薪资")]
+        public double Salary { get; set; }
+
+        [Column("部门")]
+        public string Part { get; set; }
+        [Column("ID")]
+        public int ID { get; set; }
+    }
+}
--- src/ClashDemo/ViewModels/ConnectionPageViewModel.cs
+++ src/ClashDemo/ViewModels/ConnectionPageViewModel.cs
@@ -1,6 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Bogus;
+using ClashDemo.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,11 @@ namespace ClashDemo.ViewModels
     {
         public List<string> TestItems { get; set; }
 
-        public ConnectionPageViewModel() 
+        public ObservableCollection<ConnectionPageTestModel> Datas { get; set; }
+
+        public ConnectionPageViewModel()
         {
-            TestItems = 
+            TestItems =
                 [
                 "123",
                 "456",
@@ -29,6 +34,52 @@ namespace ClashDemo.ViewModels
                 "159"
 
                 ];
+
+            Randomizer.Seed = new Random(7758);
+            int useridstart = 1;
+            var fake = new Faker("zh_CN");
+            var randomDatas = new Faker<ConnectionPageTestModel>()
+                .StrictMode(true)
+                .RuleFor(id => id.ID, f => useridstart++)
+                .RuleFor(name => name.Name, f => f.Name.FullName())
+                .RuleFor(u => u.Age, f => GetAge(f.Random.Number()) + 18)
+                .RuleFor(u => u.Part, f => f.Part())
+                .RuleFor(u => u.Salary, f =>f.Random.Double(12000,24000))
+                ;
+            Datas=new ObservableCollection<ConnectionPageTestModel>( randomDatas.Generate(50));
+            //Datas = new ObservableCollection<ConnectionPageTestModel>();
+            //Enumerable.Range(1, 50).ToList().ForEach(i => Datas.Add(new ConnectionPageTestModel
+            //{
+            //    Name = $"Name{i}",
+            //    Age = GetAge(i) + 18,
+            //    Salary = i * GetAge(i) * 1000,
+            //    Part = $"Part{i}",
+            //    ID = i
+            //}));
+        }
+
+
+
+        private int GetAge(int value)
+        {
+            return (int)value switch
+            {
+                < 20 => 1,
+                >= 20 and < 30 => 1,
+                >= 30 and < 50 => 4,
+                >= 50 and < 60 => 8,
+                >= 60 and < 70 => 12,
+                _ => 6
+            };
+        }
+    }
+
+
+    public static class bogusExtensions 
+    {
+        public static string Part(this Faker faker) 
+        {
+            return faker.PickRandom(new[] {"市场部", "人事部","总经办","宣传部","研发部"});
         }
     }
 }
